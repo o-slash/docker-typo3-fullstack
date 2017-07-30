14bc98d265497582fb2b7f296aef2d836e7d8171
Marco De Angelis <marco.deangelis@mulgo.org>
2017-07-30 13:42:06 +0200

    Fix permission issues with composer when docroot is mounted

--- run-typo3.sh
+++ run-typo3.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
-if [ -f "./composer.json" ]; then
-  if [ ! -f "./composer.lock" ]; then
-    /usr/bin/composer install;
-  else
-    /usr/bin/composer update;
-  fi
+MOUNT_GROUP=$(stat -c "%G" .)
+MOUNT_GROUP_ID=$(stat -c "%g" .)
+MOUNT_GROUP_EXISTS=$(grep "^$MOUNT_GROUP:" /etc/group | wc -l)
+
+if [ "$MOUNT_GROUP" -ne "apache" ]; then
+  groupadd -f -g $MOUNT_GROUP_ID $MOUNT_GROUP;
+  usermod -a -G "$MOUNT_GROUP" apache;
 fi
 
-chown -R apache:apache /var/www/site;
+if [ -f "./composer.json" ]; then
+  if [ ! -f "./composer.lock" ]; then
+    composer install;
+  else
+    composer update;
+  fi
+fi
 
 rm -rf /run/httpd/* /tmp/httpd*;
 exec /usr/sbin/apachectl -DFOREGROUND;
